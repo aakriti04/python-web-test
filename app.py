f669d98903aed7bd34f8d8529e2c7847fca0c7b9
aakriti04 <aakritibhardwaj04@gmail.com>
2023-08-07 16:54:27 +0000

    test

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data =[
+  {'name': 'aakriti', 'age':25},
+  {'name': 'abc', 'age':30},
+  {'name': 'mno', 'age':45},
+]
 @app.route("/")
 def hello_world():
     # return "<p>Hello, World!</p>"
-    return render_template('home.html')
+    return render_template('home.html', data=data)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug= True)
