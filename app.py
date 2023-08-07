50fede230d0c0ec9f88ec8c9717e93b2b7654c70
aakriti04 <aakritibhardwaj04@gmail.com>
2023-08-07 16:57:44 +0000

    updates

--- app.py
+++ app.py
@@ -2,15 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-data =[
-  {'name': 'aakriti', 'age':25},
-  {'name': 'abc', 'age':30},
-  {'name': 'mno', 'age':45},
+data = [
+  {
+    'name': 'aakriti',
+    'age': 25
+  },
+  {
+    'name': 'abc',
+    'age': 30
+  },
+  {
+    'name': 'mno',
+    'age': 45
+  },
 ]
+print(data)
+
+
 @app.route("/")
 def hello_world():
-    # return "<p>Hello, World!</p>"
-    return render_template('home.html', data=data)
+  # return "<p>Hello, World!</p>"
+  return render_template('home.html', data=data)
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug= True)
+  app.run(host='0.0.0.0', debug=True)
